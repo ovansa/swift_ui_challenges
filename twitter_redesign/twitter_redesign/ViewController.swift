a23d2e686f0e7990c1002d17daeee206c224708c
ovansa <aminmuhammad18@gmail.com>
2020-11-07 17:38:32 +0100

    Added setup files for twitter ui

---
+++ twitter_redesign/twitter_redesign/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  twitter_redesign
+//
+//  Created by Muhammed Ibrahim on 07/11/2020.
+//  Copyright © 2020 Ovansa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
